1eae2e8ff964aa2169ee3412ac0641e3cbf8db28
Mindy Batek <desisma@heidel.beer>
2026-02-20 15:52:25 +0100

    Fix `initializeMachineForced` not updating initialization status

--- mindyscript.d
+++ mindyscript.d
@@ -1983,6 +1983,7 @@ final class VirtualMachine(MemorySafety memorySafety = MemorySafety.system) {
 		_settings.validateThrow();
 
 		_stack = new Stack(_settings.stack);
+		_machineInitialized = true;
 	}
 
 	private void initializeMachine() {
